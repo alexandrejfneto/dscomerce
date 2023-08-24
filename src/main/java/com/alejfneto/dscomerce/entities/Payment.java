d49def594d71a45c0ebeb0aa889e919a009266df
Alexandre Ferreira <alexandrejfneto@gmail.com>
2023-08-24 16:52:28 -0300

    Create class Payment and associated whith Order

--- src/main/java/com/alejfneto/dscomerce/entities/Payment.java
+++ src/main/java/com/alejfneto/dscomerce/entities/Payment.java
@@ -3,45 +3,34 @@ package com.alejfneto.dscomerce.entities;
 import java.time.Instant;
 import java.util.Objects;
 
-import com.alejfneto.dscomerce.enums.OrderStatus;
-
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapsId;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
-@Table (name = "tb_order")
-public class Order {
+@Table (name = "tb_payment")
+public class Payment {
 	@Id
 	@GeneratedValue (strategy = GenerationType.IDENTITY)
 	private Integer id;
-	
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant moment;
-	private OrderStatus status;
-	@ManyToOne
-	@JoinColumn(name = "client_id")
-	private User client;
 	
-	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
-	private Payment payment;
+	@OneToOne
+	@MapsId
+	private Order order;
 	
-	public Order() {
+	public Payment() {
 	}
 
-	public Order(Integer id, Instant moment, OrderStatus status, User client) {
-		super();
+	public Payment(Integer id, Instant moment) {
 		this.id = id;
 		this.moment = moment;
-		this.status = status;
-		this.client = client;
 	}
 
 	public Integer getId() {
@@ -60,28 +49,12 @@ public class Order {
 		this.moment = moment;
 	}
 
-	public OrderStatus getStatus() {
-		return status;
+	public Order getOrder() {
+		return order;
 	}
 
-	public void setStatus(OrderStatus status) {
-		this.status = status;
-	}
-
-	public User getClient() {
-		return client;
-	}
-
-	public void setClient(User client) {
-		this.client = client;
-	}
-
-	public Payment getPayment() {
-		return payment;
-	}
-
-	public void setPayment(Payment payment) {
-		this.payment = payment;
+	public void setOrder(Order order) {
+		this.order = order;
 	}
 
 	@Override
@@ -97,13 +70,13 @@ public class Order {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Order other = (Order) obj;
+		Payment other = (Payment) obj;
 		return Objects.equals(id, other.id);
 	}
 
 	@Override
 	public String toString() {
-		return "Order [id=" + id + ", moment=" + moment + ", status=" + status + ", client=" + client + "]";
+		return "Payment [id=" + id + ", moment=" + moment + ", order=" + order + "]";
 	}
-	
+
 }
