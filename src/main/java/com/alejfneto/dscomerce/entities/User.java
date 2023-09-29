fc8e2cd4d7d99eda7ea7265b159580a85ccf7612
Alexandre Ferreira <alexandrejfneto@gmail.com>
2023-09-28 22:18:56 -0300

    Security implement

--- src/main/java/com/alejfneto/dscomerce/entities/User.java
+++ src/main/java/com/alejfneto/dscomerce/entities/User.java
@@ -2,20 +2,31 @@ package com.alejfneto.dscomerce.entities;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
+import java.util.Set;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
 @Table (name="tb_user")
-public class User {
+public class User implements UserDetails {
+
+	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -29,6 +40,12 @@ public class User {
 	
 	@OneToMany (mappedBy = "client")
 	private List<Order> orders = new ArrayList<>();
+	
+	@ManyToMany
+	@JoinTable(name = "tb_user_role",
+    joinColumns = @JoinColumn(name = "user_id"),
+    inverseJoinColumns = @JoinColumn(name = "role_id"))
+	private Set <Role> roles = new HashSet<>();
 	
 	public User() {
 	}
@@ -95,6 +112,14 @@ public class User {
 		return orders;
 	}
 
+	public Set<Role> getRoles() {
+		return roles;
+	}
+	
+	public void addRole (Role role) {
+		roles.add(role);
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -116,6 +141,41 @@ public class User {
 	public String toString() {
 		return "User [id=" + id + ", name=" + name + ", email=" + email + ", phone=" + phone + ", birthDate="
 				+ birthDate + ", password=" + password + "]";
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return roles;
+	}
+
+	@Override
+	public String getUsername() {
+		return email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
 	}
 	
 }
---
+++ src/main/java/com/alejfneto/dscomerce/projections/UserDetailsProjection.java
@@ -0,0 +1,10 @@
+package com.alejfneto.dscomerce.projections;
+
+public interface UserDetailsProjection {
+	
+	String getUsername();
+	String getPassword();
+	Long getRoleId();
+	String getAuthority();
+
+}
