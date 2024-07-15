4949223c15719000aa3ffccb153b55c38912ac46
Alexandre Ferreira <alexandrejfneto@gmail.com>
2024-07-15 10:23:36 -0300

    ValidateSelfOrAdmin refactored

--- src/main/java/com/alejfneto/dscomerce/services/AuthService.java
+++ src/main/java/com/alejfneto/dscomerce/services/AuthService.java
@@ -8,15 +8,22 @@ import com.alejfneto.dscomerce.services.exceptions.ForbiddenException;
 
 @Service
 public class AuthService {
+	
 	@Autowired
 	private UserService userService;
 	
 	public void validateSelfOrAdmin (Long userId) {
 		User me = userService.authenticated();
-		if (!me.hasRole("ROLE_ADMIN") && !me.getId().equals(userId)) {
-			throw new ForbiddenException ("Acesso negado!");
+//		if (!me.hasRole("ROLE_ADMIN") && !me.getId().equals(userId)) {
+//			throw new ForbiddenException ("Acesso negado!");
+//		}
+		if (me.hasRole("ROLE_ADMIN")){
+			return;
 		}
-		
+		if (!me.getId().equals(userId)) {
+			throw new ForbiddenException ("Acesso negado! Você só pode acessar seus próprios pedidos.");
+		}
+				
 	}
 
 }
